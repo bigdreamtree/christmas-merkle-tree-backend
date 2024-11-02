7460087f90ca907fae696dbf1b8d0e19b344911f
Darron Park <darron1217@gmail.com>
2024-11-02 16:08:00 +0900

    feat: add cors layer with allow all

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod utils;
 use axum::{routing::get, routing::post, Router};
 use std::net::SocketAddr;
 use crate::routes::trees::{create_tree, get_tree_messages, create_tree_message};
+use tower_http::cors::CorsLayer;
 
 #[tokio::main]
 async fn main() {
@@ -15,12 +16,13 @@ async fn main() {
     let app = Router::new()
         .route("/v1/trees", post(create_tree))
         .route("/v1/trees/:account_hash/messages", get(get_tree_messages))
-        .route("/v1/trees/:account_hash/messages", post(create_tree_message));
+        .route("/v1/trees/:account_hash/messages", post(create_tree_message))
+        .layer(CorsLayer::permissive());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    
     println!("Server running on http://{}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
